4e28d1e9838123388db6163e0ac7b713c98be6f5
aaradhya-shukla <aaradhya.shukla229@gmail.com>
2023-10-18 22:19:55 +0530

    made changes added csp/morgan/helmet for Production

--- app.js
+++ app.js
@@ -1,17 +1,59 @@
 require('dotenv').config();
+
 const express = require('express');
+
 const bodyParser = require('body-parser');
+
 const sequelize = require('./util/database');
+
 const User = require('./models/user');
+
 const SignUp = require('./routes/SignUp');
+
 const { sign } = require('crypto');
+
 const cors = require('cors');
+
 const app = express();
+
 const chat = require('./models/chats');
+
 const chatAdmin = require('./routes/chats');
+
 const Group = require('./models/Group');
+
 const Membership = require('./models/membership');
+
 const GroupAdmin = require('./routes/Group');
+
+/// new add ons->>
+const path = require('path');
+
+const fs = require('fs');
+
+const helmet = require('helmet');
+
+const morgan = require('morgan');
+
+const { expressCspHeader, INLINE, NONE, SELF } = require('express-csp-header')
+
+const accessLogStream = fs.createWriteStream(path.join(__dirname,'access.logs'),{flags:'a'});
+
+app.use(helmet());
+
+app.use(morgan('combined',{stream:accessLogStream}));
+
+app.use(expressCspHeader({ 
+    policies: { 
+        'default-src': [expressCspHeader.NONE], 
+        'img-src': [expressCspHeader.SELF], 
+    } 
+})); 
+
+
+
+////
+
 
 app.use(cors(
     {
@@ -23,7 +65,9 @@ app.use(bodyParser.json());
 app.use(SignUp);
 app.use(chatAdmin);
 app.use(GroupAdmin);
-
+app.use((req,res)=>{    
+    res.sendFile(path.join(__dirname,`public/${req.url}`))
+})
 User.hasMany(chat);
 chat.belongsTo(User);
 User.belongsToMany(Group,{through:Membership});
